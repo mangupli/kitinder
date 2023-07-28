9500046f9e55a8179aed67dcada8a0ba032dc27b
mangupli <elizaveta.mangupli@gmail.com>
2023-07-28 16:02:08 +0300

    deploy

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serverConfig = require('./config/serverConfig');
 const indexRouter = require('./routes/index.routes');
+const path = require('path');
 
 const app = express();
 const PORT = 4000;
@@ -8,5 +9,9 @@ const PORT = 4000;
 serverConfig(app);
 
 app.use('/', indexRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
